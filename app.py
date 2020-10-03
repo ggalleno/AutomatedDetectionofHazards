af24b2cc703456ad2ff08cb5b47b201f923e39d0
tanmayc07 <tanmayc32@gmail.com>
2020-10-03 09:31:37 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+# Import
+import flask
+from flask import request
+
+
+# Initialize the app
+app = flask.Flask(__name__)
+
+# Create an index route
+@app.route("/", methods=["GET"])
+def predict():
+    return flask.render_template("index.html")
+
+# Start the server
+if __name__=="__main__":
+    app.run(debug=True)
+    # For public web serving:
+    #app.run(host='0.0.0.0')
+    app.run()
