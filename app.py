ee72991d52790067fad430808266ffe6e11d2c20
tanmayc07 <tanmayc32@gmail.com>
2020-10-03 09:45:15 +0530

    Add dev branch

--- app.py
+++ app.py
@@ -11,9 +11,15 @@ app = flask.Flask(__name__)
 def predict():
     return flask.render_template("index.html")
 
+
+@app.route("/detect", methods=["GET"])
+def detect():
+    return flask.render_template("detect.html")
+
+
 # Start the server
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
     # For public web serving:
-    #app.run(host='0.0.0.0')
+    # app.run(host='0.0.0.0')
     app.run()
